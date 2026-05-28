67eb40d84e93c4721f0e6283dcc707e76768fdea
rasteadyhome <sopocedu@gmail.com>
2026-05-28 03:47:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('RA Digital Finance is live!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
